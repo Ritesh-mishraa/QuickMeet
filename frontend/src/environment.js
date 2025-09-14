9b470f183cbcec6dc70f8e85b08acefbcb4c8da8
Ritesh kumar mishra <ritzzmi01@gmail.com>
2025-09-14 15:26:36 +0530

    Update environment.js

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,7 +1,7 @@
 let IS_PROD = true; // Change this to true for production
 
 const servers = IS_PROD
-  ? "https://quickmeet-mnac.onrender.com/"
+  ? "https://quickmeet-mnac.onrender.com"
   : "http://localhost:8000";
 
 export default servers;
