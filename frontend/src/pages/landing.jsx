2a0dd6b642f44f1b0eccbaa6800470b5ecafdecc
Ritesh-mishraa <rajmi8948360380@gmail.com>
2025-09-19 00:35:22 +0530

    Adding responsiveness in the landing page

--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "../App.css";
 import "../styles/landingPage.css";
+import { ReactTyped } from "react-typed";
 
 export default function landing() {
   return (
@@ -17,11 +18,19 @@ export default function landing() {
         </div>
       </nav>
       <div className="landingPageMainContainer">
-        <div>
-          <h1>
-            <span style={{ color: "#FF9839" }}>Connect</span> With Your Loved
-            One
+        <div className="landingPageTextContainer">
+          <h1 className="text-5xl font-bold text-white">
+            <ReactTyped
+              strings={[
+                 "<span style='color:#FF9839'>Connect</span> With Your<br/>Loved One"
+              ]}
+              typeSpeed={50}
+              backSpeed={30}
+              smartBackspace
+              loop={false}
+            />
           </h1>
+
           <p>Cover a distance by Quick Meet</p>
           <div>
             <Link to={"/auth"}>
@@ -29,7 +38,7 @@ export default function landing() {
             </Link>
           </div>
         </div>
-        <div>
+        <div className="landingPageImageContainer">
           <img src="/mobile.png" alt="" />
         </div>
       </div>
